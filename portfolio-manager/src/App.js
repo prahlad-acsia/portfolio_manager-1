e1a86748f28a366eaed7c836bdcf4b8dc1187171
Prahlad BK <prahladbk50@gmail.com>
2024-06-18 12:00:35 +0530

    s

--- portfolio-manager/src/App.js
+++ portfolio-manager/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>portfolio1</h1>
+        <h1>portfoli</h1>
       </header>
     </div>
   );
